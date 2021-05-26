c0cea26e9ae181141afb12b63470d7e2cc2b7ff6
Skencker <Sandrinekencker@hotmail.com>
2021-05-26 14:16:35 +0200

    page actualité + parents + doc

---
+++ app.js
@@ -0,0 +1,10 @@
+// const image_reunion = document.querySelector('.image-reunion');
+
+
+// window.onscroll=function() {
+//     console.log(image_reunion);
+//     image_reunion.style.positon = 'absolute';
+//     image_reunion.style.left = '800px';
+//     image_reunion.style.top = '150px';
+//     image_reunion.style.animation = 'slide-in-right 1.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both';
+// }
